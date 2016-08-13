20fafe797e305fee71efd3ceff0d10fbac755495
Bill So <hokchun@gmail.com>
2016-08-14 04:50:45 +0800

    Fixes for Xcode 8 beta 5

---
+++ FakerMac/Faker.h
@@ -0,0 +1,19 @@
+//
+//  FakerMac.h
+//  FakerMac
+//
+//  Created by Bill So on 8/13/16.
+//  Copyright © 2016 Apostle. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Faker.
+FOUNDATION_EXPORT double FakerVersionNumber;
+
+//! Project version string for Faker.
+FOUNDATION_EXPORT const unsigned char FakerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FakerMac/PublicHeader.h>
+
+
